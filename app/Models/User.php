61803095ab421f58eb94bc8be4980d6546f75ff1
emanuel gtz <sswemanuel@gmail.com>
2026-01-10 22:41:04 -0600

    add relationship with vacation model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +45,37 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /** ========================
+     * ACCESSORS
+     * =========================
+     */
+
+    /** ========================
+     * MUTATORS
+     * =========================
+     */
+
+    /** ========================
+     * LOCAL SCOPES
+     * =========================
+     */
+
+    /** ========================
+     * EVENTS
+     * =========================
+     */
+
+    /** ========================
+     * RELATIONSHIPS
+     * =========================
+     */
+    /**
+     * Retrieves the vacations record of the current user
+     */
+    public function vacation(): HasOne
+    {
+        return $this->hasOne(Vacation::class);
     }
 }
