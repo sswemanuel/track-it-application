627a61c495c268d670afd7939c78259ef0f79963
emanuel gtz <sswemanuel@gmail.com>
2026-01-10 21:11:56 -0600

    install pint library to format the code of the project

--- app/Models/Vacation.php
+++ app/Models/Vacation.php
@@ -4,9 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
+/**
+ * Register vacation days of users
+ */
 class Vacation extends Model
 {
-    /** @use HasFactory<\Database\Factories\VacationFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'spent_days',
+        'total_days',
+        'user_id',
+    ];
+
+    /** ========================
+     * ACCESSOR
+     * =========================
+     */
+    /**
+     * Returns remaining vacations days of current user
+     */
+    protected function remainingVacationDays(): Attribute {
+       return Attribute::make(
+            get: fn (int $value, array $attributes) => $attributes['spent_days'] - $attributes['total_days']
+        );
+    }
+
+    /** ========================
+     * MUTATORS
+     * =========================
+     */
+
+    /** ========================
+     * EVENTS
+     * =========================
+     */
+
+    /** ========================
+     * RELATIONSHIPS
+     * =========================
+     */
 }
--- database/migrations/2026_01_11_015921_create_vacations_table.php
+++ database/migrations/2026_01_11_015921_create_vacations_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('vacations', function (Blueprint $table) {
             $table->id();
+            $table->integer('spent_days');
+            $table->integer('total_days');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
